e420b1c0bbd2c1b380bd98bd28297a88c13c077b
Koutaku <koakutsu.k@gmail.com>
2019-02-20 22:20:40 +0900

    最初のcommit

---
+++ src/main/java/samplekoakutsu/Main.java
@@ -0,0 +1,11 @@
+package samplekoakutsu;
+
+public class Main {
+	public static void main(String sasa[]) {
+		for(int i=0; i<2; i++) {
+		System.out.println(sasa[i]);
+		}
+		System.out.println("Hello World");
+	}
+
+}
